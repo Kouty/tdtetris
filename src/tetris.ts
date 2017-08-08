061e0a376eff6f84c9530d98128d107cee99c996
Kouty <kouty79@gmail.com>
2017-08-08 17:08:16 +0200

    Test on Tetris spawn after lock

--- src/tetris.ts
+++ src/tetris.ts
@@ -5,13 +5,22 @@ export class Tetris {
     private generator: TetrominoGenerator;
     private playField: PlayField;
 
-    constructor() {
+    constructor(numRows: number, numCols: number) {
         this.generator = new TetrominoGenerator();
-        this.playField = new PlayField(20, 10);
+        this.playField = new PlayField(numRows, numCols);
     }
 
     public start(): void {
         const tetromino = this.generator.next();
         this.playField.spawn(tetromino);
+    }
+
+    public moveDown() {
+        const moved = this.playField.tetromino.moveDown();
+
+        if (!moved) {
+            const tetromino = this.generator.next();
+            this.playField.spawn(tetromino);
+        }
     }
 }
---
+++ test/tetris.spec.ts
@@ -0,0 +1,39 @@
+import {Tetris} from '../src/tetris';
+import {PlayField} from '../src/playfield';
+import {TetrominoGenerator} from '../src/tetrominoGenerator';
+
+describe('Tetris', function () {
+    let oneSquareTetromino;
+    beforeEach(function () {
+        oneSquareTetromino = {width: 1, height: 1, filledSquares: () => [{row: 0, col: 0}]};
+    });
+
+    it('should spawn first tetromino on start', function () {
+        const controller = new Tetris(20, 10);
+        const firstTetromino = {};
+        spyOn(TetrominoGenerator.prototype, 'next').and.returnValue(firstTetromino);
+        spyOn(PlayField.prototype, 'spawn').and.returnValue(firstTetromino);
+
+        controller.start();
+
+        expect(TetrominoGenerator.prototype.next).toHaveBeenCalled();
+        expect(PlayField.prototype.spawn).toHaveBeenCalledWith(firstTetromino);
+    });
+
+    it('should spawn a new tetromino, when current tetromino locks', function () {
+        const controller = new Tetris(1, 3);
+        let counter = 0;
+        const tetrominoes = [
+            (Object as any).assign({}, oneSquareTetromino),
+            (Object as any).assign({}, oneSquareTetromino)];
+        spyOn(TetrominoGenerator.prototype, 'next').and.callFake(function () {
+            return tetrominoes[counter++];
+        });
+        spyOn(PlayField.prototype, 'spawn').and.callThrough();
+
+        controller.start();
+        controller.moveDown();
+
+        expect(PlayField.prototype.spawn).toHaveBeenCalledWith(tetrominoes[1]);
+    });
+});
