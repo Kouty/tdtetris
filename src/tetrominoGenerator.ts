8c0af99f8803c49c79e78e18f57baa157f8a6354
Andrea Koutifaris <kouty79@gmail.com>
2017-08-13 12:37:42 +0200

    Added spec for tetromino generator for O tetromino, fixed linting problems

--- src/tetrominoGenerator.ts
+++ src/tetrominoGenerator.ts
@@ -2,6 +2,16 @@ import {ITetromino, Tetrominoes} from './tetromino';
 
 export class TetrominoGenerator {
     public next(): ITetromino {
-        return Tetrominoes.I.create();
+        const tetrominos = Object.keys(Tetrominoes);
+        const rand = getRandomInt(0, tetrominos.length);
+
+        return Tetrominoes[tetrominos[rand]].create();
     }
+}
+
+// The maximum is exclusive and the minimum is inclusive
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
 }
--- test/tetrominoGenerator.spec.ts
+++ test/tetrominoGenerator.spec.ts
@@ -1,15 +1,30 @@
-import {TetrominoGenerator} from '../src/tetrominoGenerator';
 import {Tetrominoes} from '../src/tetromino';
+import {TetrominoGenerator} from '../src/tetrominoGenerator';
 
 describe('TetrominoGenerator', function () {
 
-    it('should generate I tetrominoes (until the game is I tetris)', function () {
+    beforeEach(function () {
+        spyOn(Math, 'random');
+    });
+
+    it('should generate I tetromino ', function () {
+        Math.random.and.returnValue(0);
         const tetrominoGenerator = new TetrominoGenerator();
         spyOn(Tetrominoes.I, 'create');
 
         tetrominoGenerator.next();
 
         expect(Tetrominoes.I.create).toHaveBeenCalled();
+    });
+
+    it('should generate O tetromino ', function () {
+        const tetrominoGenerator = new TetrominoGenerator();
+        Math.random.and.returnValue(0.5);
+        spyOn(Tetrominoes.O, 'create');
+
+        tetrominoGenerator.next();
+
+        expect(Tetrominoes.O.create).toHaveBeenCalled();
     });
 
 });
