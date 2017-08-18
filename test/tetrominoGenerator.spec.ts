ed097e82380191fab2c247fa575439aa9ec2a041
Kouty <kouty79@gmail.com>
2017-08-18 14:37:51 +0200

    Refactored TetrominoGenerator specs to generic test for all

--- test/tetrominoGenerator.spec.ts
+++ test/tetrominoGenerator.spec.ts
@@ -6,35 +6,16 @@ describe('TetrominoGenerator', function () {
         spyOn(Math, 'random');
     });
 
-    it('should generate I tetromino ', function () {
-        (Math.random as any).and.returnValue(0);
-        const tetrominoGenerator = new TetrominoGenerator();
-        spyOn(Tetrominoes.I, 'create');
-
-        tetrominoGenerator.next();
-
-        expect(Tetrominoes.I.create).toHaveBeenCalled();
-    });
-
-    it('should generate O tetromino ', function () {
-        const tetrominoGenerator = new TetrominoGenerator();
-        const threshold =  1 / Object.keys(Tetrominoes).length;
-        (Math.random as any).and.returnValue(threshold);
-        spyOn(Tetrominoes.O, 'create');
-
-        tetrominoGenerator.next();
-
-        expect(Tetrominoes.O.create).toHaveBeenCalled();
-    });
-
     it('should generate T tetromino ', function () {
         const tetrominoGenerator = new TetrominoGenerator();
-        const threshold =  2 / Object.keys(Tetrominoes).length;
-        (Math.random as any).and.returnValue(threshold);
-        spyOn(Tetrominoes.T, 'create');
+        Object.keys(Tetrominoes).forEach((key, index, array) => {
+            const threshold = index / array.length;
+            (Math.random as any).and.returnValue(threshold);
+            spyOn(Tetrominoes[key], 'create');
 
-        tetrominoGenerator.next();
+            tetrominoGenerator.next();
 
-        expect(Tetrominoes.T.create).toHaveBeenCalled();
+            expect(Tetrominoes[key].create).toHaveBeenCalled();
+        });
     });
 });
