6b0b79b667fd037b45e89333c0ad082d33aee170
Kouty <kouty79@gmail.com>
2017-08-24 17:15:18 +0200

    Created ScoreCalculator

---
+++ src/scoreCalculator.ts
@@ -0,0 +1,19 @@
+export class ScoreCalculator {
+    private pScore: number;
+
+    constructor() {
+        this.pScore = 0;
+    }
+
+    public addPointsForLock() {
+        this.pScore++;
+    }
+
+    public addPointsForClear(numRowsCleared: number) {
+        this.pScore += (numRowsCleared * (numRowsCleared + 1) / 2) * 10;
+    }
+
+    get score() {
+        return this.pScore;
+    }
+}
---
+++ test/score.calculator.spec.ts
@@ -0,0 +1,34 @@
+import {ScoreCalculator} from '../src/scoreCalculator';
+
+describe('ScoreCalculator', () => {
+    let calculator;
+
+    beforeEach(function() {
+        calculator = new ScoreCalculator();
+    });
+
+    it('should add 1 to score for each lock', () => {
+        calculator.addPointsForLock();
+
+        expect(calculator.score).toBe(1);
+    });
+
+    it('should add 10 for 1 row cleared', () => {
+        calculator.addPointsForClear(1);
+
+        expect(calculator.score).toBe(10);
+    });
+
+    it('should add 10 + 20 for 2 rows cleared', () => {
+        calculator.addPointsForClear(2);
+
+        expect(calculator.score).toBe(30);
+    });
+
+    it('should add the sum of cleared rows * 10 points for N rows cleared', () => {
+        calculator.addPointsForClear(4);
+
+        expect(calculator.score).toBe(100);
+    });
+
+});
